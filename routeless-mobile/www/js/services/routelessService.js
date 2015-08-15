b1f7ad99dcc1231480e8fe2cc45b5c838317a774
schoolie <brian.p.schoolcraft@gmail.com>
2015-08-15 16:09:18 -0400

    updated to use brianschoolcraft.com server

--- routeless-mobile/www/js/services/routelessService.js
+++ routeless-mobile/www/js/services/routelessService.js
@@ -6,14 +6,14 @@ var routelessServices = angular.module('routelessServices', ['ngResource']);
 
 routelessServices.factory('User', ['$resource',
   function($resource){
-    return $resource('http://192.168.1.65:5000/api_1_0/users_/:id', {id:'@id'}, {
+    return $resource('http://brianschoolcraft.com:5000/api_1_0/users_/:id', {id:'@id'}, {
         query: {method:'GET', isArray:false}
     });
   }]);
 
 routelessServices.factory('Course', ['$resource',
   function($resource){
-    return $resource('http://localhost:5000/api_1_0/courses/:id', {id:'@id'}, {
+    return $resource('http://brianschoolcraft.com:5000/api_1_0/courses/:id', {id:'@id'}, {
         query: {
           method:'GET',
           isArray:false,
@@ -60,7 +60,7 @@ routelessServices.factory('Course', ['$resource',
 
 routelessServices.factory('CheckPoint', ['$resource',
   function($resource){
-    return $resource('http://localhost:5000/api_1_0/checkpoints/:id', {id:'@id'}, {
+    return $resource('http://brianschoolcraft.com:5000/api_1_0/checkpoints/:id', {id:'@id'}, {
         query: {method:'GET', isArray:false},
         update: {method: 'PUT'}  
     });
