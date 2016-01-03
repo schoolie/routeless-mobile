8e967e4dcab3b241a0892e48e0650321d6c58998
schoolie <brian.p.schoolcraft@gmail.com>
2016-01-02 21:19:51 -0500

    creating GeoJSON layers from checkpoints, events attached

--- routeless-mobile/www/js/controllers/EventDetailCtrl.js
+++ routeless-mobile/www/js/controllers/EventDetailCtrl.js
@@ -29,18 +29,34 @@ routelessControllers.controller('EventDetailCtrl',
             }
             cp_idx++;
           });
-          console.log(found_idx);
           return found_idx;
         };
+        
+        // Checkpoint click callback
+        var layerClickCallback = function(e) {
+          var active_id = e.target.feature.properties.id;
+          active_idx = cp_idx_from_id(active_id);
+          $scope.active_marker = $scope.event.course.check_points[active_idx];
+          $scope.$apply();
+          $scope.createLogPoint(active_idx);
+        };
+        
+        // Function to bind click callbacks to GeoJSON points
+        var onEachFeature = function(feature, layer) {
+          //bind click
+          layer.on('click', layerClickCallback);
+        };
+
         /**
          * Once state loaded, put map on scope.
          */
         $scope.$on("$stateChangeSuccess", function() {
-
+          
           angular.extend($scope, {
+            active_marker: {id: 'test'},
             lfevents: {
               map: {
-                  enable: [],
+                  enable: ['click'],
                   logic: 'emit'
               },
               markers: {
@@ -74,42 +90,97 @@ routelessControllers.controller('EventDetailCtrl',
                     url: "http://s3-us-west-1.amazonaws.com/caltopo/topo/{z}/{x}/{y}.png?v=1",
                     type: 'xyz'
                   }
-              }
-            },
-            paths: {circle: {
-                type: "circle",
-                radius: 50,
-                weight: 1,
-                latlngs:  {
-                  lat: 40.442,
-                  lng: -86.909
+              },
+              overlays: {
+                range_circles: {
+                  name:'Range Circles',
+                  type: 'geoJSONShape',
+                  data: {
+                    type: "FeatureCollection",
+                    features: []
+                  },
+                  visible: true,
+                  layerOptions: {
+                    pointToLayer: function (geojson, latlng) {
+                      return L.circle(latlng, 50, {
+                        color: '#0f0',
+                        weight: 2,
+                        fill: false,
+                        clickable: false
+                      });
+                    }
+                  }
+                },
+                
+                checkpoints: {
+                  name:'Check Points',
+                  type: 'geoJSONShape',
+                  data: {
+                    type: "FeatureCollection",
+                    features: []
+                  },
+                  visible: true,
+                  layerOptions: {
+                    pointToLayer: function (geojson, latlng) {
+                      return L.circleMarker(latlng, {
+                        color: '#33f',
+                        weight: 2,
+                        fillOpacity: 1
+                      });
+                    },
+                    onEachFeature: onEachFeature
+                  }
                 }
               }
-            }
+            }      
           });
-
-
+          
+          console.log($scope);
           
           var active_id;
           var active_idx;
           
-          var markerEvents = leafletMarkerEvents.getAvailableEvents();
-          for (var k in markerEvents){
-              var eventName = 'leafletDirectiveMarker.myMap.' + markerEvents[k];
-              $scope.$on(eventName, function(event, args){
-                if (event.name === 'leafletDirectiveMarker.myMap.click') {
-//                  $scope.eventDetected = event.name;
-                  active_id = args.leafletEvent.target.options.id;
-                  active_idx = cp_idx_from_id(active_id);
-                  $scope.active_marker = $scope.event.course.check_points[active_idx];
-                  $scope.createLogPoint(active_idx);
-                }
-              });
-          }
+//          var markerEvents = leafletMarkerEvents.getAvailableEvents();
+//          for (var k in markerEvents){
+//              var eventName = 'leafletDirectiveMarker.myMap.' + markerEvents[k];
+//              $scope.$on(eventName, logPointCallback);
+//          }
           
           $scope.event = Event.query({id: $stateParams.eventId}, success=function() {
             console.log('success');
+//            $scope.layers.overlays.checkpoints.data.features.geometry = 
+            var features = [];
+            
+            $scope.event.course.check_points.forEach(function(cp) {
+              
+              $scope.layers.overlays.range_circles.data.features.push({
+                type: "Feature",
+                properties: {
+                  title: cp.title,
+                  description: cp.message
+                },
+                geometry: {
+                  type: "Point",
+                  coordinates: [cp.lng, cp.lat]
+                }
+              });
+
+              $scope.layers.overlays.checkpoints.data.features.push({
+                type: "Feature",
+                properties: {
+                  id: cp.id,
+                  title: cp.title,
+                  description: cp.message
+                },
+                geometry: {
+                  type: "Point",
+                  coordinates: [cp.lng, cp.lat]
+                }
+              });
+            });
+            
             console.log($scope.event);
+            
           });          
           
         });
