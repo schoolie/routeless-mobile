d699ed7d87d2081e0e37e10748747e63c562bc98
schoolie <brian.p.schoolcraft@gmail.com>
2016-01-04 09:02:22 -0500

    condensed GeoJSON creation, working on logpoint display

--- routeless-mobile/www/js/controllers/EventDetailCtrl.js
+++ routeless-mobile/www/js/controllers/EventDetailCtrl.js
@@ -47,6 +47,53 @@ routelessControllers.controller('EventDetailCtrl',
           layer.on('click', layerClickCallback);
         };
 
+
+        var createGeoJSON = function(name, marker_func) {
+          var GeoJSON = {
+                  name: name,
+                  type: 'geoJSONShape',
+                  data: {
+                    type: "FeatureCollection",
+                    features: []
+                  },
+                  visible: true,
+                  layerOptions: {
+                    pointToLayer: function (feature, latlng) {
+                      return marker_func(feature, latlng);
+                    },
+                    onEachFeature: onEachFeature
+                  }
+                };
+          return GeoJSON;
+        };
+        
+        var addLogPoint = function(lp) {
+                
+          var color = '#f00';
+          if (lp.type === 'found') {
+            color = '#00f';
+          }
+          else if (lp.type === 'near') {
+            color = '#ffff00'
+          }
+
+          var point_data = {
+            type: "Feature",
+            properties: {
+              distance: lp.distance,
+              color: color
+            },
+            geometry: {
+              type: "Point",
+              coordinates: [lp.lng, lp.lat]
+            }
+          };
+
+          $scope.layers.overlays.logpoints.data.features.push(point_data);
+
+
+        };
+        
         /**
          * Once state loaded, put map on scope.
          */
@@ -92,50 +139,59 @@ routelessControllers.controller('EventDetailCtrl',
                   }
               },
               overlays: {
-                range_circles: {
-                  name:'Range Circles',
-                  type: 'geoJSONShape',
-                  data: {
-                    type: "FeatureCollection",
-                    features: []
-                  },
-                  visible: true,
-                  layerOptions: {
-                    pointToLayer: function (geojson, latlng) {
-                      return L.circle(latlng, 50, {
-                        color: '#0f0',
-                        weight: 2,
-                        fill: false,
-                        clickable: false
-                      });
-                    }
-                  }
-                },
                 
-                checkpoints: {
-                  name:'Check Points',
-                  type: 'geoJSONShape',
-                  data: {
-                    type: "FeatureCollection",
-                    features: []
-                  },
-                  visible: true,
-                  layerOptions: {
-                    pointToLayer: function (geojson, latlng) {
-                      return L.circleMarker(latlng, {
+                found_circles: createGeoJSON('Found Circles', function(feature, latlng) {
+                    return L.circle(latlng, feature.properties.found_distance, {
+                      color: '#0f0',
+                      weight: 2,
+                      fill: false,
+                      clickable: false
+                    });
+                  }
+                ),                
+                near_circles: createGeoJSON('Near Circles', function(feature, latlng) {
+                    return L.circle(latlng, feature.properties.near_distance, {
+                      color: '#ffff00',
+                      weight: 2,
+                      fill: false,
+                      clickable: false
+                    });
+                  }
+                ),                
+                checkpoints: createGeoJSON('Check Points', function(feature, latlng) {
+                    return L.circleMarker(latlng, {
+                      color: '#33f',
+                      weight: 2,
+                      fillOpacity: 1
+                    });
+                  }
+                ),                
+                logpoints: createGeoJSON('Log Points', function(feature, latlng) {
+                    return L.circleMarker(latlng, {
+                      color: feature.properties.color,
+                      weight: 2,
+                      radius: 5,
+                      fillOpacity: 1
+                    });
+                  }
+                ),                    
+                labels: createGeoJSON('Labels', function(feature, latlng) {
+                  return L.marker(latlng, {
                         color: '#33f',
                         weight: 2,
-                        fillOpacity: 1
+                        fillOpacity: 1,
+                        icon: L.divIcon({
+                          className: 'label',
+                          html: feature.properties.title,
+                          iconSize: [50, 40]
+                        })
                       });
-                    },
-                    onEachFeature: onEachFeature
-                  }
-                }
+                    }
+                )                
               }
             }      
           });
           
-          console.log($scope);
           
           var active_id;
           var active_idx;
@@ -149,34 +205,33 @@ routelessControllers.controller('EventDetailCtrl',
           $scope.event = Event.query({id: $stateParams.eventId}, success=function() {
             console.log('success');
 //            $scope.layers.overlays.checkpoints.data.features.geometry = 
-            var features = [];
             
+            // Add Check Point info to GeoJSON Layers
             $scope.event.course.check_points.forEach(function(cp) {
               
-              $scope.layers.overlays.range_circles.data.features.push({
+              var point_data = {
                 type: "Feature",
                 properties: {
                   title: cp.title,
-                  description: cp.message
+                  description: cp.message,
+                  near_distance: cp.near_distance,
+                  found_distance: cp.found_distance
                 },
                 geometry: {
                   type: "Point",
                   coordinates: [cp.lng, cp.lat]
                 }
-              });
-
-              $scope.layers.overlays.checkpoints.data.features.push({
-                type: "Feature",
-                properties: {
-                  id: cp.id,
-                  title: cp.title,
-                  description: cp.message
-                },
-                geometry: {
-                  type: "Point",
-                  coordinates: [cp.lng, cp.lat]
-                }
-              });
+              };
+              
+              $scope.layers.overlays.found_circles.data.features.push(point_data);
+              $scope.layers.overlays.near_circles.data.features.push(point_data);
+              $scope.layers.overlays.checkpoints.data.features.push(point_data);
+              $scope.layers.overlays.labels.data.features.push(point_data);
+              
+            });
+            
+            $scope.event.check_point_logs.forEach(function(cpl) {
+              cpl.log_points.forEach(addLogPoint);
             });
             
             console.log($scope.event);
@@ -194,18 +249,32 @@ routelessControllers.controller('EventDetailCtrl',
       
               targLatLng = L.latLng(target.lat, target.lng);
               currLatLng = L.latLng(position.coords.latitude, position.coords.longitude);
+                            
+              $scope.markers = [currLatLng];
               
               var distance = currLatLng.distanceTo(targLatLng);
               console.log(distance);
               
+              var type = '';
+              var found = 0;
+              if (distance <= target.found_distance) {
+                type = 'found';
+                found = 1;
+              }
+              else if (distance <= target.near_distance) {
+                type = 'near';
+              }
+              console.log(type);
               var log_point = new LogPoint({
                 check_point_log_id: $scope.event.check_point_logs[active_idx].id,
                 lat: position.coords.latitude,
                 lng: position.coords.longitude,
                 distance: distance,
-                type: 'found'
+                type: type
               });
               log_point.$save();
+              
+              addLogPoint(log_point);
 
               $scope.event.check_point_logs[active_idx].log_points.push(log_point);
               
@@ -214,6 +283,5 @@ routelessControllers.controller('EventDetailCtrl',
               console.log("Location error!");
               console.log(err);
             });
-
         };
       }]);
